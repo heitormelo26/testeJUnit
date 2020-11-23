286ca58fde8f313a3cc555f9e711960ae028ff07
Heitor Melo <37666760+heitormelo26@users.noreply.github.com>
2020-11-23 18:37:58 -0300

    Add files via upload

---
+++ teste-de-unidade/src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -0,0 +1,39 @@
+package br.com.caelum.leilao.teste;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+
+import br.com.caelum.leilao.servico.Avaliador;
+
+public class TesteDoAvaliador {
+    
+	@Test
+	public void main() {
+        // cenario: 3 lances em ordem crescente
+        Usuario joao = new Usuario("João");
+        Usuario jose = new Usuario("José");
+        Usuario maria = new Usuario("Maria");
+
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(joao, 250.0));
+        leilao.propoe(new Lance(jose, 300.0));
+        leilao.propoe(new Lance(maria, 400.0));
+
+        // executando a acao
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+        
+        double maiorEsperado = 400;
+        double menorEsperado = 250;
+
+        // exibindo a saida
+
+        Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+        Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+    }
+}
